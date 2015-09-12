af28ee3e52747f250932da0d101292bffd86d309
kibray <marcogigliarano@Marcos-MacBook-Pro.local>
2015-09-12 00:43:49 -0300

    fix no http module

--- src/hackfridaynight.ts
+++ src/hackfridaynight.ts
@@ -1,12 +1,19 @@
 /// <reference path="./typings/angular2/angular2.d.ts"/>
+
 import {Component, View, bootstrap} from 'angular2/angular2';
+
+import {HTTP_BINDINGS} from 'http/http';
+
+import {Friendship} from './friendship';
 
 @Component({
     selector: 'hackfridaynight-app',
-    injectables: []})
+    injectables: [],
+    viewBindings: [HTTP_BINDINGS]
+})
 @View({
     templateUrl: 'hackfridaynight.html',
-    directives: []
+    directives: [Friendship]
 })
 class HackfridaynightApp {
     constructor() {
